8f280b595294a62614eaf27d3ff5062987935114
mikaflo <mikafloh9@gmail.com>
2025-11-17 18:45:43 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$link1 = "https://google.com";
+$link2 = "https://bing.com";
+?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include 'config.php';
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WATCHBEST VIDEO</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div style="width: 100vw;height: 100vh;overflow: hidden;">
+        <svg style="width: 100%;height: 100%;" role="presentation" viewBox="0 0 1403 1092" preserveAspectRatio="xMidYMid slice"><defs><linearGradient id="__id0__id1" gradientUnits="userSpaceOnUse" x1="0" y1="546" x2="1403" y2="546"><stop stop-color="#0cc0df" stop-opacity="1" offset="0"></stop><stop stop-color="#ffde59" stop-opacity="1" offset="1"></stop></linearGradient></defs><rect x="0" y="0" width="1403" height="1092" fill="url(#__id0__id1)"></rect></svg>
+        <div class="w-100 h-100 position-fixed top-0 gap-3 d-flex flex-column justify-content-center align-items-center">
+            <div class="mb-3">
+                <span class="title">WATCH BEST VIDEO</span>
+            </div>
+            <div class="mb-3">
+                <span class="title2">Melayu Lucah</span>
+            </div>
+            <div>
+                <div class="mb-3">
+                    <a href="<?= $link1; ?>" class="btn btn-primary btn-custom">BEST MALAY</a>
+                </div>
+                <div class="mb-3">
+                    <a href="<?= $link2; ?>" class="btn btn-primary btn-custom">HOT MALAY</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
